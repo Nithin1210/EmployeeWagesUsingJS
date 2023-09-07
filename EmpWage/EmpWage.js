b4525c8b31aaac1f089624c42216e5ffe129bafd
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-09-07 14:42:17 +0530

    [Nithin] Add : Updation of Employee Wage

--- EmpWage/EmpWage.js
+++ EmpWage/EmpWage.js
@@ -1,8 +1,31 @@
-const IS_AbBSENT = 0; //UC-1
-let empcheck = Math.floor(Math.random() * 10) % 2;
-if (empcheck == IS_AbBSENT) {
+const IS_AbBSENT=0;
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HR=4;
+const Full_TIME_HR=8;
+const WAGE_PER_HR=20;
+
+
+let empcheck = Math.floor(Math.random()*10)%2;  //UC-1
+if(empcheck==IS_AbBSENT)
+{
     console.log("Employee is absent");
 }
-else {
-    console.log("Employee is Present");
+else
+{
+console.log("Employee is Present");
 }
+
+
+let emphrs=0;                                   //UC-2
+empcheck=Math.floor(Math.random()*10)%3;
+switch(empcheck)
+{
+    case IS_PART_TIME: emphrs=PART_TIME_HR;
+    break;
+    case IS_FULL_TIME: emphrs=Full_TIME_HR;
+    break;
+    case IS_AbBSENT: emphrs=IS_AbBSENT
+}
+let empwage= emphrs*WAGE_PER_HR;
+console.log("EmpWage is "+empwage);
